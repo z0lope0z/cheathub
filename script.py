6218f16828e33434f659c28653d09a3df6361571
Lope Emano <lopegwapo@gmail.com>
2013-07-04 12:04:32 +0800

    added a clean feature wherein it will clean the repo once the script runs out of links added binary output to .gitignore

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ config.read('setup.cfg')
 
 
 class URLCrawler:
+    url = ''
     urls = {}
     done = []
     
@@ -23,6 +24,7 @@ class URLCrawler:
         os.chdir('..')
     
     def crawl_site(self, url):
+        self.url = url
         response = urllib.urlopen(url)
         soup = BeautifulSoup(response.read())
         code_block = soup.find_all(href=re.compile(".py$"))
@@ -35,18 +37,16 @@ class URLCrawler:
     
     def load_new(self):
         #is_valid if script was able to download something
-        is_valid = False
         for key in self.urls:
             if not self._check_done(key):
                 file = urllib.urlretrieve(self.urls[key])
                 os.system('mkdir files/%s' % (key))
                 os.system('wget --directory-prefix=files/%s %s' % (key, self.urls[key]))
-                is_valid = True
-                break
+                return key
             else:
                 print "File name %s already exists" % key
-        return is_valid
-
+        return None
+    
     def _check_done(self, file_name):
         if not self.done:
             files = os.listdir('files')
@@ -58,7 +58,7 @@ class URLCrawler:
         if file_name in self.done:
             return True
         return False
-
+    
     def push_git(self, message):
         os.chdir('files/')
         os.system('git pull origin master')
@@ -67,15 +67,18 @@ class URLCrawler:
         os.system('git push origin master')
         os.chdir('..')
         self._update_last_update()
-
+    
     def clean(self):
         files = os.listdir('files')
         os.chdir('files/')
+        files.remove('.git')
+        print 'deleting files %s' % files
         for file in files:
             os.system('rm -rf %s/' % file)
         os.chdir('..')
         self.push_git("cleared the files")
-
+        self.urls = {}
+    
     def _update_last_update(self):
         last_date = config.get('config', 'last_date')
         datetime.strptime(last_date, '%b %d %Y %I:%M%p')
@@ -87,10 +90,10 @@ class URLCrawler:
 source_sites = []
 source_sites = config.get('links', 'urls').split(',')
 crawler = URLCrawler()
-if not crawler.load_new():
+crawler.crawl_site(source_sites[0])
+new_file = crawler.load_new()
+if not new_file:
     crawler.clean()
+    crawler.crawl_site(source_sites[0])
     crawler.load_new()
-crawler.clean()
-#crawler.crawl_site(source_sites[0])
-#crawler.load_new()
-crawler.push_git("initial commit")
+crawler.push_git("push a new file %s" % new_file)
